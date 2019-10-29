32194696700084b6c0dd6dc524186b100f658085
Giorgi Papakerashvili <s8pagior@stud.uni-saarland.de>
2019-10-29 14:10:02 +0100

    Add admin for Find model

--- kaunos/app/admin.py
+++ kaunos/app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from app.models import Area
 from app.models import Trench
 from app.models import Building
+from app.models import Find
 
 class AreaAdmin(admin.ModelAdmin):
     pass
@@ -12,6 +13,10 @@ class TrenchAdmin(admin.ModelAdmin):
 class BuildingAdmin(admin.ModelAdmin):
     pass
 
+class FindAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Area, AreaAdmin)
 admin.site.register(Trench, TrenchAdmin)
 admin.site.register(Building, BuildingAdmin)
+admin.site.register(Find, FindAdmin)
