47895c889a78d34b944516a044678d5ab549fe0a
Giorgi Papakerashvili <s8pagior@stud.uni-saarland.de>
2019-10-29 13:12:06 +0100

    Added Area in models and in admins

--- kaunos/app/admin.py
+++ kaunos/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Area
 
-# Register your models here.
+class AreaAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Area, AreaAdmin)
--- kaunos/app/models.py
+++ kaunos/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Area(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
