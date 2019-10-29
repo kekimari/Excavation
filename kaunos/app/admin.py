a2c56f7e878f69fe5a02b2d10451adfa02d6aba9
Giorgi Papakerashvili <s8pagior@stud.uni-saarland.de>
2019-10-29 13:52:46 +0100

    Added Trench model; Added some migrations

--- kaunos/app/admin.py
+++ kaunos/app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from app.models import Area
+from app.models import Trench
 
 class AreaAdmin(admin.ModelAdmin):
     pass
 
+class TrenchAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Area, AreaAdmin)
+admin.site.register(Trench, TrenchAdmin)
--- kaunos/app/models.py
+++ kaunos/app/models.py
@@ -5,3 +5,14 @@ class Area(models.Model):
 
     def __str__(self):
         return self.name
+
+class Trench(models.Model):
+    name = models.CharField(max_length=250)
+    editor = models.CharField(max_length=250)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+
+    # TODO: Add support for files
+
+    def __str__(self):
+        return self.name
