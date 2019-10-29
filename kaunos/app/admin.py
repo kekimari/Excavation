b6c9227a3558bb55fad495f6e94ff989db73d441
Giorgi Papakerashvili <s8pagior@stud.uni-saarland.de>
2019-10-29 14:01:43 +0100

    Added admin for Buildng model

--- kaunos/app/admin.py
+++ kaunos/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from app.models import Area
 from app.models import Trench
+from app.models import Building
 
 class AreaAdmin(admin.ModelAdmin):
     pass
@@ -8,5 +9,9 @@ class AreaAdmin(admin.ModelAdmin):
 class TrenchAdmin(admin.ModelAdmin):
     pass
 
+class BuildingAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Area, AreaAdmin)
 admin.site.register(Trench, TrenchAdmin)
+admin.site.register(Building, BuildingAdmin)
